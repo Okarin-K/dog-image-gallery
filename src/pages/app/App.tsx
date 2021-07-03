a3182b36414534641262330d07fc94f928c9f97d
Sazanamin <rachel9029@icloud.com>
2021-07-04 04:27:37 +0900

    add Todoアプリ

---
+++ src/pages/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { TodoApp } from '../Todo/TodoApp';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Todoリスト</h1>
+            <TodoApp />
+        </div>
+    );
+}
+
+export default App;
